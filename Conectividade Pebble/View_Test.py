506a32acaebcd20aa6cbbfd30fbe61cc32e5292f
Roberto Victor <robertovictor_rj@hotmail.com>
2020-07-21 03:00:01 -0300

    Revert "Initial commit"
    This reverts commit d7d7a32c645f88f8e9d9ca5eb57fb6a3a252f083.

--- Conectividade Pebble/View_Test.py
+++
@@ -1,8 +0,0 @@
-import pypyodbc
-
-connection = pypyodbc.connect('Driver={SQL Server};'
-                              'Server=localhost;'
-                              'Database=testdb;'
-                              'uid=sa;pwd=P@ssw0rd')
-
-
--- GetMemoryLoad.py
+++
@@ -1,9 +0,0 @@
-import psutil
-
-
-f = open('UsedMem.txt', 'w')
-f.write(str(psutil.virtual_memory().used))
-f.close()
-
-
-
